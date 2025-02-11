2337fd9cc1ff84b165bae532776cb42e655d0afe
code-toni <antoniolorenzofroncillo@gmail.com>
2025-02-11 13:26:02 +0100

    template

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Congratulations!</h1>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/**/*.blade.php',
+        './resources/js/**/*.vue',
+    ],
+
+    safelist: [
+        {
+            pattern: /bg-(slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(50|100|200|300|400|500|600|700|800|900|950)/
+        },
+
+        {
+            pattern: /border-(slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(50|100|200|300|400|500|600|700|800|900|950)/
+        }
+    ]
+};
